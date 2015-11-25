3ea6bed10a018a034d9c6225803cee2b3fb2f76d
Reiuiji <reiuiji@gmail.com>
2015-11-25 16:49:25 -0500

    Updated part1 to calculate average with a group by

--- FinalExam/part1.sql
+++ FinalExam/part1.sql
@@ -10,12 +10,13 @@ PROMPT ================================
 --Note: Run @table.sql @values.sql prior to see results
 -- Assume only Stock for fish species for avg num in stock & stock value
 
-SELECT scientific_name, number_in_stock AS stock_num, (number_in_stock*current_price) AS stock_value
+SELECT scientific_name, AVG(number_in_stock) AS stock_num , AVG(number_in_stock*current_price) AS stock_value
 FROM aquatic_organism_stock
 WHERE number_in_stock > 0
 AND NOT EXISTS ( -- Select any organism other than invertebrates
     SELECT * FROM marine_invertebrates
-    WHERE marine_invertebrates.scientific_name = aquatic_organism_stock.scientific_name);
+    WHERE marine_invertebrates.scientific_name = aquatic_organism_stock.scientific_name)
+GROUP BY aquatic_organism_stock.scientific_name;
 
 
 --total sum of available stock of all fish species
